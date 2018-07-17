f3da87aee7f05aba7164b8b65647f38dc7d1933a
Dae <juliandaeone@gmail.com>
2018-07-17 16:02:04 +1000

    add login

--- routes/api/users.js
+++ routes/api/users.js
@@ -51,4 +51,29 @@ router.post("/register", (req, res) => {
   //access it with req.body
 });
 
+// @route           GET api/users/login
+// @description     login user / returning JWT token
+// @access          Public route
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // find user by email
+  User.findOne({ email }).then(user => {
+    // check for user
+    if (!user) {
+      return res.status(404).json({ email: "user not found!" });
+    }
+
+    // check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Success" });
+      } else {
+        return res.status(400).json({ password: "password incorrect" });
+      }
+    });
+  });
+});
+
 module.exports = router;
